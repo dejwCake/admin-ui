23e0efe785f02369649afe41303747fc1e4479d2
David Běhal <david.behal@websupport.sk>
2025-01-04 16:25:36 +0100

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
+use Brackets\AdminUI\Http\Controllers\WysiwygMediaUploadController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web', 'admin'])->group(static function (): void {
-    Route::namespace('Brackets\AdminUI\Http\Controllers')->group(static function (): void {
-        Route::post('/admin/wysiwyg-media', 'WysiwygMediaUploadController@upload')->name(
-            'brackets/admin-ui::wysiwyg-upload',
-        );
+Route::middleware(['web', 'admin'])
+    ->prefix('admin')
+    ->group(static function (): void {
+        Route::post('/wysiwyg-media', [WysiwygMediaUploadController::class, 'upload'])
+            ->name('brackets/admin-ui::wysiwyg-upload');
     });
-});
