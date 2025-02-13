24d533a16bf3eacfa08bc1175d844662794b6440
David Běhal <david.behal@team.blue>
2025-02-13 17:45:33 +0100

    Unset type

--- src/Console/Commands/AdminUIInstall.php
+++ src/Console/Commands/AdminUIInstall.php
@@ -90,6 +90,10 @@ class AdminUIInstall extends Command
             $packageJsonContent['devDependencies']['laravel-mix'] = '^6.0.6';
         }
 
+        if (isset($packageJsonContent['type'])) {
+            unset($packageJsonContent['type']);
+        }
+
         $packageJsonContent['devDependencies']['@dejwcake/craftable'] = '^0.1.8';
         $packageJsonContent['devDependencies']['vue-loader'] = '^15.9.8';
         $packageJsonContent['devDependencies']['sass-loader'] = '^8.0.2';
